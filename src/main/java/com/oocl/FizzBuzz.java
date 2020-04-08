bae9ce9b2befa898af7eb8550ae9a1d3aaa5de9b
chowca <56864611+chowca@users.noreply.github.com>
2020-04-08 15:41:45 +0800

    [Calvin Chow] passed the test should_return_Fizz_when_raw_number_is_divisible_by_3

--- src/main/java/com/oocl/FizzBuzz.java
+++ src/main/java/com/oocl/FizzBuzz.java
@@ -2,6 +2,9 @@ package com.oocl;
 
 public class FizzBuzz {
     public String say(int number){
-        return String.valueOf(number);
+        String result = "";
+        if (number % 3 == 0)
+            result += "Fizz";
+        return result.equals("") ? String.valueOf(number):result;
     };
 }
